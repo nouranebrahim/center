89f4ce068797ae8270d9e97d1e72371897cd9c18
nouranebrahim <nouranebrahim9@gmail.com>
2020-10-16 23:21:18 +0200

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
 // Auth::routes(['verify'=>true]);
 
 Route::get('/courses', [ CourseController::class, 'index'])->name('courses.index'); 
